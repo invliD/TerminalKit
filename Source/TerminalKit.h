dfef09e043a31743db1520eee13ad6cc7b0c0edd
Sebastian Brückner <mail@invlid.com>
2015-08-15 20:43:46 -0700

    Initial commit.

---
+++ Source/TerminalKit.h
@@ -0,0 +1,7 @@
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TerminalKit.
+FOUNDATION_EXPORT double TerminalKitVersionNumber;
+
+//! Project version string for TerminalKit.
+FOUNDATION_EXPORT const unsigned char TerminalKitVersionString[];
